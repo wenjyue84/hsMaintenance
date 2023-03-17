65dc433fc1997b9b6ccfd34e53196a217aa0edd3
wenjyue84 <68142625+wenjyue84@users.noreply.github.com>
2023-03-17 09:04:13 +0800

    Initial commit
    first version generated by Chatgpt

---
+++ app.css
@@ -0,0 +1,85 @@
+/* General styles */
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f2f2f2;
+  }
+  
+  /* Styles for maintenance form */
+  form {
+    max-width: 500px;
+    margin: 0 auto;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  form label {
+    display: block;
+    margin-bottom: 10px;
+    font-weight: bold;
+  }
+  
+  form input[type="text"],
+  form textarea {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 20px;
+    border: none;
+    border-radius: 3px;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+  }
+  
+  form textarea {
+    height: 150px;
+  }
+  
+  form button {
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 3px;
+    cursor: pointer;
+  }
+  
+  /* Styles for maintenance status */
+  .status {
+    margin: 20px auto;
+    max-width: 500px;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  .status h2 {
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+  
+  .status ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  
+  .status li {
+    margin-bottom: 10px;
+  }
+  
+  .status .completed {
+    color: green;
+    font-weight: bold;
+  }
+  
+  .status .in-progress {
+    color: orange;
+    font-weight: bold;
+  }
+  
+  .status .not-started {
+    color: red;
+    font-weight: bold;
+  }
+  
